c2078fe03fee797edf44a63a6cc04b0ce6006c82
finile <wu.nile@gmail.com>
2018-01-10 22:40:51 +0800

    add move new and create actions and views for admin

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -6,4 +6,25 @@ class Admin::MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      flash[:notice] = "Movie was sucessfully created"
+      redirect_to admin_movies_path
+    else
+      flash.now[:alert] = "Movie was failed to create"
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :score, :description )
+  end
+
 end
