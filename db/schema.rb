301977e627abb1c93b3758cbae6261b4c7e2c1a4
finile <wu.nile@gmail.com>
2018-01-13 20:08:46 +0800

    add column image to movie table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180109140537) do
+ActiveRecord::Schema.define(version: 20180113120658) do
 
   create_table "movies", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180109140537) do
     t.integer "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
